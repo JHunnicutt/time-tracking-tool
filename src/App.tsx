563145ea817404181dc788b44e211d6438ca5553
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-01-16 12:11:09 -0500

    add basic implementation of useReducer()

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import Form from './components/Form/Form'
 import DataTable from './components/DataTable/DataTable'
 import './App.css'
 
 import defaultFormData from './components/Form/dataModel'
 
+type ACTIONTYPE = 
+  | {type: 'name'; payload: string}
+
+function reducer(state: typeof defaultFormData, action: ACTIONTYPE) {
+  switch (action.type) {
+    case 'name' : 
+      return { name: action.payload }
+  }
+}
+
+
 function App() {
 
-  const [formData, setFormData] = useState(defaultFormData)
+  // const [formData, setFormData] = useState(defaultFormData)
+  const [state, dispatch] = useReducer(reducer, defaultFormData)
   const [tableDisplay, setTableDisplay] = useState(false)
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault()
+    console.log(state)
     setTableDisplay(true)
   }
 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      name: event.target.value
-    })
+    // setFormData({
+    //   name: event.target.value
+    // })
+    dispatch({type: 'name', payload: event.target.value})
   }
 
-
+  console.log(state)
   return (
     <main>
       <h1>React Time Tracking Tool</h1>
       <Form
-        formData={formData}
+        formData={state}
         handleSubmit={(event) => submitHandler(event)}
         handleChange={(event) => changeHandler(event)}
       />
       {
         tableDisplay ?
         <DataTable 
-          tableData={formData}
+          tableData={state}
         /> :
         null
       }
